2210559dc63797c087073c11adc52fc7feebc1fd
Michael Douglas <michaelperezdouglas@gmail.com>
2020-02-01 13:51:16 -0500

    Added script to controller repairer main game

---
+++ Controller Repair/Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaderboard : MonoBehaviour
+{
+    public string[] names = new string[10];
+    public float number = 0f;
+
+    public void Start()
+    {
+         while(number < 1);
+         string randomArrayString = names [Random.Range(0, names.Length)];
+         number = number + .1f;
+         Debug.Log(randomArrayString);
+
+    }
+
+}
