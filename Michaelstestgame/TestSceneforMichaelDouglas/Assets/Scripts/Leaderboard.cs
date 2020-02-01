a94663db58cc3ceed052b3f194d52283298a0651
Michael Douglas <michaelperezdouglas@gmail.com>
2020-02-01 13:41:11 -0500

    Leaderboard modification

--- Michaelstestgame/TestSceneforMichaelDouglas/Assets/Scripts/Leaderboard.cs
+++ Michaelstestgame/TestSceneforMichaelDouglas/Assets/Scripts/Leaderboard.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    public string[] names = new string[10];
+    public string[] names = [new string[10]];
     public float number = 0f;
 
     public void Start()
