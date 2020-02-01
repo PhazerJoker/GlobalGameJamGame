91e267503ea3eb26b5541966c185bf1dad24f13a
Michael Douglas <michaelperezdouglas@gmail.com>
2020-02-01 12:45:37 -0500

    Changes to test game

--- Michaelstestgame/TestSceneforMichaelDouglas/Assets/Scripts/Leaderboard.cs
+++ Michaelstestgame/TestSceneforMichaelDouglas/Assets/Scripts/Leaderboard.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Leaderboard : MonoBehaviour
 {
-    public string[] names = new string[10000];
+    public string[] names = new string[10];
+    public float number = 0f;
+
+    public void Start()
+    {
+         while(number < 1);
+         string randomArrayString = names [Random.Range(0, names.Length)];
+         number = number + .1f;
+         Debug.Log(randomArrayString);
+
+    }
+
+
+
+
+
+
 }
